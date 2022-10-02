350babc062f973b202fc67c2e76dc5bfe622d590
Gastón Aragón <gaston.ara.o@gmail.com>
2022-10-01 22:29:24 -0300

    navbar responsive

--- public/assets/icons/iconoAboutMe.js
+++ public/assets/icons/iconoAboutMe.js
@@ -4,7 +4,7 @@ const IconoAboutMe = () => {
   return (
     <>
     <svg className={navbar.icons} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M7.5 6.5C7.5 8.981 9.519 11 12 11s4.5-2.019 4.5-4.5S14.481 2 12 2S7.5 4.019 7.5 6.5zM20 21h1v-1c0-3.859-3.141-7-7-7h-4c-3.86 0-7 3.141-7 7v1h17z"/></svg>
-    <span>Sobre mí</span>
+    <span className={navbar.textIcons}>Sobre mí</span>
     </>
   )
 }
--- public/assets/icons/iconoHabilidades.js
+++ public/assets/icons/iconoHabilidades.js
@@ -4,7 +4,7 @@ const IconoHabilidades = () => {
   return (
     <>
     <svg className={navbar.icons} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 32 32"><path fill="currentColor" d="M30 30h-8V4h8zm-10 0h-8V12h8zm-10 0H2V18h8z"/></svg>
-    <span>Habilidades</span>
+    <span className={navbar.textIcons}>Habilidades</span>
     </>
     )
 }
