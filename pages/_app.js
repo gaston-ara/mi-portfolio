04780d8ea737f37c859ba904888340ab00da2c15
Gastón Aragón <gaston.ara.o@gmail.com>
2022-10-02 22:34:25 -0300

    Probando atributo preload

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,28 @@
 import '../styles/globals.css'
 import { ContextProvider } from '../context/musicContext'
 import Layout from '../components/layout'
+import { useEffect } from 'react'
+import * as ga from '../lib/ga'
+import {useRouter} from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+     //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <ContextProvider><Layout><Component {...pageProps} /></Layout></ContextProvider> 
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,23 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                {/* Google tag (gtag.js) */}
+            <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet" />
